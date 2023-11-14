be96eea84a1b891c4f214ba377e093c3c9b0d74b
Omowunmi Kamiludeen <balikiskamil@gmail.com>
2023-11-14 09:38:45 +0100

    Basic application

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,19 +1,17 @@
-import logo from './holberton.jpg';
+import logo from './holberton-favicon.ico';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} alt='logo' />
+    <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
-      </header>
-      <body className='App-body'>
-        <p>Login to access the full dashboard</p>
-      </body>
-      <footer className='App-footer'>
-        <p>Copyright 2020 - holberton School</p>
-      </footer>
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
+        </div>
+        <div className="App-footer">
+          <p>Copyright 2020 - holberton School</p>
+        </div>
     </div>
   );
 }
